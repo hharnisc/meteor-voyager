6ee30bf17f0dd1874ea182770e6cb03842f4a78b
Harrison Harnisch <hharnisc@gmail.com>
2014-08-28 21:08:20 -0700

    updates for 0.9.0

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-  summary: 'voyager'
+  summary: 'voyager',
+  version: "0.1.1",
+  git: "https://github.com/hharnisc/meteor-voyager.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.use('coffeescript');
-    api.use('server-stats', 'server');
+    api.use("mrt:server-stats@0.1.0", 'server');
     api.export('Voyager', 'server');
 
   // Generated with: github.com/philcockfield/meteor-package-loader
